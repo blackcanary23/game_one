2ddf55cf8f3540fc8b8e3a9b67a5d0dec5e7cea8
blackcanary23 <46376028+blackcanary23@users.noreply.github.com>
2019-02-27 20:36:13 +0300

    Create br.h

---
+++ br.h
@@ -0,0 +1,15 @@
+int SIZE;
+//int new_SIZE;
+
+typedef struct
+{
+	int strategy_number_of_B;
+	int min_element;
+} gamer_B;
+
+typedef struct
+{
+	int strategy_number_of_A;
+	int max_element;
+} gamer_A;
+
