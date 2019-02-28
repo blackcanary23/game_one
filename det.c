126df2b0012308650122e4736fb1e12e3927831b
blackcanary23 <46376028+blackcanary23@users.noreply.github.com>
2019-02-28 17:27:13 +0300

    Update det.c

--- det.c
+++ det.c
@@ -4,20 +4,24 @@
 
 
 // Получение матрицы без i-й строки и j-го столбца
-void get_matrix(float** matrix, float** new_matrix, int row, int column, int SIZE) {
+void get_matrix(float** matrix, float** new_matrix, int row, int column, int SIZE) 
+{
 	int di, dj;
 	di = 0;
-	for (int i = 0; i < SIZE - 1; i++) { // проверка индекса строки
+	for (int i = 0; i < SIZE - 1; i++) 
+	{ // проверка индекса строки
 		if (i == row) di = 1;
 		dj = 0;
-		for (int j = 0; j < SIZE - 1; j++) { // проверка индекса столбца
+		for (int j = 0; j < SIZE - 1; j++) 
+		{ // проверка индекса столбца
 			if (j == column) dj = 1;
 			new_matrix[i][j] = matrix[i + di][j + dj];
 		}
 	}
 }
 // Рекурсивное вычисление определителя
-int compute_determinant(float** matrix, int SIZE) {
+int compute_determinant(float** matrix, int SIZE) 
+{
 	int det, sign, new_SIZE;
 
 	float **new_matrix;
@@ -31,16 +35,20 @@ int compute_determinant(float** matrix, int SIZE) {
 	new_SIZE = SIZE - 1;
 	if (SIZE < 1)
 		printf("Определитель вычислить невозможно!");
-	if (SIZE == 1) {
+	if (SIZE == 1) 
+	{
 		det = matrix[0][0];
 		return(det);
 	}
-	if (SIZE == 2) {
+	if (SIZE == 2) 
+	{
 		det = matrix[0][0] * matrix[1][1] - (matrix[1][0] * matrix[0][1]);
 		return(det);
 	}
-	if (SIZE > 2) {
-		for (int i = 0; i < SIZE; i++) {
+	if (SIZE > 2) 
+	{
+		for (int i = 0; i < SIZE; i++) 
+		{
 			get_matrix(matrix, new_matrix, i, 0, SIZE);
 			det = det + sign * matrix[i][0] * compute_determinant(new_matrix, new_SIZE);
 			sign = -sign;
