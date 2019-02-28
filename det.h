90fcd564c82a9d41edb0fb51c27fa99b02e213c4
blackcanary23 <46376028+blackcanary23@users.noreply.github.com>
2019-02-28 16:23:12 +0300

    Update det.h

--- det.h
+++ det.h
@@ -1,5 +1,10 @@
+#ifndef DET_H
+#define DET_H
 
 
 int compute_determinant(float **matrix, int SIZE);
 
 void get_matrix(float** matrix, float** new_matrix, int row, int column, int SIZE);
+
+
+#endif
